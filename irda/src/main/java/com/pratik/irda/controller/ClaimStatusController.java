8e513ef27619369fb2e4ca71a388851d0c41ebb9
Pratikraj777 <131874651+Pratikraj777@users.noreply.github.com>
2023-06-05 09:42:27 +0530

    Add files via upload

---
+++ irda/src/main/java/com/pratik/irda/controller/ClaimStatusController.java
@@ -0,0 +1,28 @@
+package com.pratik.irda.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pratik.irda.entities.ClaimStatus;
+import com.pratik.irda.service.ClaimStatusService;
+
+@RestController
+public class ClaimStatusController {
+    @Autowired
+    ClaimStatusService claimStatusService;
+    @GetMapping("irda/claimStatus/repot/{month}/{year}")
+    List<ClaimStatus> getClaimStatusByMonthYear(@PathVariable String month,@PathVariable int year)
+    {
+        return claimStatusService.getClaimStatusByMonthYear(month,year);
+    }
+    
+    @GetMapping("irda/claimStatus/pull/<month>/<year>")
+    public ClaimStatus getStatusOfClaimsByMonth(@PathVariable String month, @PathVariable int year) {
+    	return null;
+    }
+    
+}
